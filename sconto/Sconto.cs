2cc42d6538a41e6f20d737cd7f161f5f789e7608
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-18 13:29:26 +0200

    Sviluppo

---
+++ sconto/Sconto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Controllo
+{
+    public class Sconto
+    {
+        public static double Saldo (double importo_primo, double importo_secondo)
+        {
+            if (importo_primo < importo_secondo)
+            {
+                double sconto = (importo_primo / 100) * 50;
+                importo_primo = importo_primo - sconto;
+                double importo_totale = importo_primo + importo_secondo;
+                return importo_totale;
+            }
+            else if (importo_primo > importo_secondo)
+            {
+                double sconto = (importo_secondo / 100) * 50;
+                importo_secondo = importo_secondo - sconto;
+                double importo_totale = importo_primo + importo_secondo;
+                return importo_totale;
+            }
+        }
+    }
+}
--- terzo_negozio/Program.cs
+++ terzo_negozio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Controllo;
 
 namespace terzo_negozio
 {
@@ -14,22 +15,8 @@ namespace terzo_negozio
             importo_primo = double.Parse(Console.ReadLine());
             Console.Write("Inserisci il prezzo del secondo prodotto: ");
             importo_secondo = double.Parse(Console.ReadLine());
-            if (importo_primo < importo_secondo)
-            {
-                sconto = (importo_primo / 100) * 50;
-                importo_primo = importo_primo - sconto;
-                importo_totale = importo_primo + importo_secondo;
-                Console.WriteLine($"L'importo totale è {importo_totale}");
-            }
-            else if (importo_primo > importo_secondo)
-            {
-                sconto = (importo_secondo / 100) * 50;
-                importo_secondo = importo_secondo - sconto;
-                importo_totale = importo_primo + importo_secondo;
-                Console.WriteLine($"L'importo totale è {importo_totale}");
-            }
+            importo_totale = Sconto.Saldo(importo_primo, importo_secondo);
             Console.ReadLine();
-
         }
     }
 }
